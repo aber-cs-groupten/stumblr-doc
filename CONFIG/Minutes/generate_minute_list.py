893881bbd0167f6c096c0cc8f021bddb0a8d6675
Charles Newey <charlie@assemblyco.de>
2013-11-02 00:53:37 +0000

    Added script to generate minutes lists

---
+++ CONFIG/Minutes/generate_minute_list.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# Run this every time you add minutes
+
+import os
+
+minutes_list = ["List of minutes for Group 10", "---------------------------"]
+minutes_number = 0
+for filename in os.listdir("./Minutes"):
+    if os.path.isdir(filename):
+        continue
+
+    minutes_number += 1
+    line = ("  * [Meeting {0}]({1})".format(minutes_number, filename))
+    minutes_list.append(line)
+
+with open("minute_list.txt", 'w') as f:
+    for line in minutes_list:
+        print line
+        f.write(line)
+
+print "Minute list generated."
