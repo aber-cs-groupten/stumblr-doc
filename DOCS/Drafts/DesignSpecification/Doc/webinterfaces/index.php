daac4f7732139d4a0e58807fac8d5338d94144a3
Charles Newey <charlie@assemblyco.de>
2014-02-15 19:45:43 +0000

    Reorganised structure

--- DOCS/Drafts/DesignSpecification/Doc/webinterfaces/index.php
+++ DOCS/Drafts/DesignSpecification/Doc/webinterfaces/index.php
@@ -3,10 +3,9 @@ $walkID = 0;
 if(isset($_POST['tour'])){
 	$walkID = $_POST['tour'];
 }
-else{
-}
+
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED );
-$con = mysql_connect ("db.dcs.aber.ac.uk", "admcsgp10", "73GRlj5m");
+$con = mysql_connect ("db.dcs.aber.ac.uk", "username", "password");
 $db = mysql_query("USE csgp10_13_14", $con);
 $res = mysql_query ("select * from walks where id='$walkID'");
 $query = mysql_query ("SELECT * FROM location WHERE walkID='$walkID'");
@@ -14,14 +13,10 @@ $query2 = mysql_query ("SELECT * FROM description WHERE walkID='$walkID'");
 
 
 $marker[] = 0;
- ?>
+?>
 
-
-//
 //    HTML code that generates webpage
 //    HTML includes the google map functionality
-//
-
 
 <?php
 while ($a = mysql_fetch_array ($res))
@@ -44,9 +39,4 @@ echo "<p>" . $a["description"] . "</p>";
 }
 ?>
 
-//
 //	HTML code that generates table to structure the page
-//
-//
-
-	
